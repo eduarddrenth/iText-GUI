fc64dd82c8a478cb160413e6c1d0d1a820e2171f
eduarddrenth <eduarddrenth@gmail.com>
2016-07-10 08:29:24 +0200

    fix styling problem on to new reporting lib

--- src/main/java/com/vectorprint/vectorprintreportgui/StylesheetBuilder.java
+++ src/main/java/com/vectorprint/vectorprintreportgui/StylesheetBuilder.java
@@ -10,17 +10,16 @@ package com.vectorprint.vectorprintreportgui;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -63,7 +62,7 @@ public class StylesheetBuilder extends Application {
    public void stop() throws Exception {
       System.exit(0);
    }
-   
+
    public static void main(String[] args) throws IOException {
       if (new File(LOGGINGPROPERTIES).canRead()) {
          LogManager.getLogManager().readConfiguration(new FileInputStream(LOGGINGPROPERTIES));
@@ -73,5 +72,5 @@ public class StylesheetBuilder extends Application {
       }
       launch(args);
    }
-   public static final String LOGGINGPROPERTIES = System.getProperty("user.dir")+File.separator+"VectorPrintGUILogging.properties";
+   public static final String LOGGINGPROPERTIES = System.getProperty("user.dir") + File.separator + "VectorPrintGUILogging.properties";
 }
