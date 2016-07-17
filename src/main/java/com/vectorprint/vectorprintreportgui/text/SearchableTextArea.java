64ddc69e6742ed9b49e16a4b55e4fd148aecfede
eduarddrenth <eduarddrenth@gmail.com>
2016-07-17 14:11:10 +0200

    searching more characters search mode styling

--- src/main/java/com/vectorprint/vectorprintreportgui/text/SearchableTextArea.java
+++ src/main/java/com/vectorprint/vectorprintreportgui/text/SearchableTextArea.java
@@ -1,6 +1,7 @@
 package com.vectorprint.vectorprintreportgui.text;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -51,6 +52,19 @@ public class SearchableTextArea extends AnchorPane {
 
    private boolean scroll;
 
+   private static boolean supportedInSearch(KeyCode keyCode) {
+      return KeyCode.SEMICOLON == keyCode || KeyCode.COLON == keyCode || KeyCode.COMMA == keyCode
+          || KeyCode.EXCLAMATION_MARK == keyCode || KeyCode.EQUALS == keyCode || KeyCode.UNDERSCORE == keyCode
+          || KeyCode.UNDERSCORE == keyCode || KeyCode.PERIOD == keyCode || KeyCode.MINUS == keyCode
+          || KeyCode.BRACELEFT == keyCode || KeyCode.BRACERIGHT == keyCode || KeyCode.OPEN_BRACKET == keyCode
+          || KeyCode.CLOSE_BRACKET == keyCode || KeyCode.ASTERISK == keyCode || KeyCode.AMPERSAND == keyCode
+          || KeyCode.LEFT_PARENTHESIS == keyCode || KeyCode.RIGHT_PARENTHESIS == keyCode || KeyCode.PLUS == keyCode
+          || KeyCode.QUOTE == keyCode || KeyCode.QUOTEDBL == keyCode || KeyCode.BACK_QUOTE == keyCode
+          || KeyCode.BACK_SLASH == keyCode || KeyCode.SLASH == keyCode || KeyCode.DOLLAR == keyCode
+          || KeyCode.EURO_SIGN == keyCode || KeyCode.POUND == keyCode || KeyCode.GREATER == keyCode
+          || KeyCode.LESS == keyCode || KeyCode.STAR == keyCode;
+   }
+
    private void searchTxt(KeyEvent event) {
       KeyCode kc = event.getCode();
 
@@ -59,7 +73,7 @@ public class SearchableTextArea extends AnchorPane {
       String s = searchText.getText();
       boolean again = false;
 
-      if (kc.isLetterKey() || kc.isDigitKey() || kc.isWhitespaceKey()) {
+      if (kc.isLetterKey() || kc.isDigitKey() || kc.isWhitespaceKey() || supportedInSearch(kc)) {
          s += event.getText();
       } else {
          switch (kc) {
@@ -125,21 +139,30 @@ public class SearchableTextArea extends AnchorPane {
          // start search in stylesheet
          if (isFindMode()) {
             // stop searching
-            text.getStyleClass().remove("searching");
+            searchStyle(null);
             searchText.setText("");
             setFindMode(false);
          } else {
             // start searching
-            text.getStyleClass().add("searching");
+            searchStyle(null);
             setFindMode(true);
          }
       } else if (KeyCode.ESCAPE == event.getCode()) {
          // stop searching
-         text.getStyleClass().remove("searching");
+         searchStyle(null);
          searchText.setText("");
          setFindMode(false);
       } else if (isFindMode()) {
          searchTxt(event);
+      }
+   }
+
+   @FXML
+   private void searchStyle(ActionEvent event) {
+      if (findMode.isSelected()) {
+         text.getStyleClass().add("searching");
+      } else {
+         text.getStyleClass().remove("searching");
       }
    }
 
